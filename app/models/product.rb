61cc738f9fb0be491ecd5bf10864cedf9f3ba95e
akhil <akhilyadav.com@gmail.com>
2021-09-12 20:06:29 +0530

    Implement cart system

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,16 @@
 class Product < ApplicationRecord
 
   validates :name, :price, presence:true
-  
+
   def to_s
     name
+  end
+
+  def to_builder
+    Jbuilder.new do |product|
+      product.price stripe_price_id
+      product.quantity 1
+    end
   end
 
   after_create do
